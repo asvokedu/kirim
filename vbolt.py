016833ab0a0ba843697a71ae3d9b927de2c584ce
asvokedu <asepvukis@gmail.com>
2025-07-16 23:43:37 -0700

    Update vbolt.py

--- vbolt.py
+++ vbolt.py
@@ -14,7 +14,7 @@ from rich.live import Live
 from rich.panel import Panel
 from rich.text import Text
 
-# Setup logging
+# Setup logging,
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s',
